179905ee3b8c11efb988ec20ebc1ec8af4dffc0e
sonylomo <sonylomo1@gmail.com>
2021-06-20 06:00:44 +0000

    navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Navbar from "../components/Navbar"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
+const Home = () => {
   return (
+    <>
       <Head>
         <title>ChRIS Store</title>
         <meta name="description" content="ChRIS Store landing page" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-  )
-}
+      <Navbar/>
+    </>
+  );
+};
+
+export default Home;
